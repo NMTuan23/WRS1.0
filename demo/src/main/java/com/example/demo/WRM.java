ebf5b36848604a34b601aab6ec83bca8871df236
NMTuan23 <105837223+NMTuan23@users.noreply.github.com>
2024-10-07 18:53:44 +0700

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/WRM.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WRM {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WRM.class, args);
+	}
+
+}
